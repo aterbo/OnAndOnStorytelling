cdcc44434407f8af16166e7943edaf830c65b66d
ATerbo <andy.terbovich@gmail.com>
2016-02-09 18:36:23 -0800

    Created record voice activity

---
+++ app/src/main/java/com/aterbo/tellme/RecordStory.java
@@ -0,0 +1,13 @@
+package com.aterbo.tellme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordStory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_story);
+    }
+}
