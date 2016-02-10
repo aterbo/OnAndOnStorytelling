6350512ee4981ff4226886794f69bcababf50aae
ATerbo <andy.terbovich@gmail.com>
2016-02-09 22:27:19 -0800

    Added new Activity for picking one of three topics to record

---
+++ app/src/main/java/com/aterbo/tellme/PickTopicToRecord.java
@@ -0,0 +1,13 @@
+package com.aterbo.tellme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickTopicToRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_topic_to_record);
+    }
+}
