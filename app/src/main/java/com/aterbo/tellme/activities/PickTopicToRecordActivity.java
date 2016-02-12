d9bd13c0353de5431e785b29d4e87ce98d54efad
ATerbo <andy.terbovich@gmail.com>
2016-02-12 15:06:50 -0800

    Refactored activities into activities package

---
+++ app/src/main/java/com/aterbo/tellme/activities/PickTopicToRecordActivity.java
@@ -0,0 +1,42 @@
+package com.aterbo.tellme.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.aterbo.tellme.R;
+
+public class PickTopicToRecordActivity extends AppCompatActivity {
+
+    String chosenTopic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_topic_to_record);
+        chosenTopic = new String();
+    }
+
+    public void topic1(View view){
+        chosenTopic = ((Button)findViewById(R.id.topic_1)).getText().toString();
+        startNextActivity();
+    }
+
+    public void topic2(View view){
+        chosenTopic = ((Button)findViewById(R.id.topic_2)).getText().toString();
+        startNextActivity();
+    }
+
+    public void topic3(View view){
+        chosenTopic = ((Button)findViewById(R.id.topic_3)).getText().toString();
+        startNextActivity();
+    }
+
+    private void startNextActivity(){
+        Intent intent = new Intent(this, RecordStoryActivity.class);
+        intent.putExtra("ChosenTopic", chosenTopic);
+        startActivity(intent);
+    }
+}
