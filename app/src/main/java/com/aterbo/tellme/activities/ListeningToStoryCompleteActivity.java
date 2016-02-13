4dca71108dfdff300056ca834ead856a95ab4a19
ATerbo <andy.terbovich@gmail.com>
2016-02-13 00:29:16 -0800

    Created new ListeningToStoryActivity and put header data in layout

---
+++ app/src/main/java/com/aterbo/tellme/activities/ListeningToStoryCompleteActivity.java
@@ -0,0 +1,15 @@
+package com.aterbo.tellme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aterbo.tellme.R;
+
+public class ListeningToStoryCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listening_to_story_complete);
+    }
+}
