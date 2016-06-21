e01547b315be1de87f67b0c807b916b0b44aed7b
ATerbo <andy.terbovich@gmail.com>
2016-06-20 19:11:45 -0700

    Put permissions checking logic in SplashScreen

--- app/src/main/java/com/onanon/app/activities/SplashScreen.java
+++ app/src/main/java/com/onanon/app/activities/SplashScreen.java
@@ -1,5 +1,12 @@
 package com.onanon.app.activities;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,9 +14,91 @@ import com.onanon.app.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+
+    private final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (isPermissionsGranted()) {
+            moveOnToNextActivity();
+        } else {
+            requestAppPermissions();
+        }
+    }
+
+    private boolean isPermissionsGranted(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }  else {
+            return false;
+        }
+    }
+
+    private void requestAppPermissions(){
+        if (isShowExplainationRequired()){
+            showPermissionsExplainationDialog("You need to allow access to the microphone to record " +
+                            "your awesome stories!",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            showPermissionsRequestDialog();
+                        }
+                    });
+        } else {
+            showPermissionsRequestDialog();
+        }
+    }
+
+    private boolean isShowExplainationRequired(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
+            return true;
+        }  else {
+            return false;
+        }
+    }
+
+    private void showPermissionsRequestDialog(){
+        ActivityCompat.requestPermissions(SplashScreen.this,
+                new String[]{Manifest.permission.RECORD_AUDIO},
+                MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_RECORD_AUDIO: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    moveOnToNextActivity();
+                } else {
+                    requestAppPermissions();
+                }
+                return;
+            }
+        }
+    }
+
+    private void moveOnToNextActivity(){
+        Intent intent = new Intent(this, OpeningScreen.class);
+        startActivity(intent);
+    }
+
+    private void showPermissionsExplainationDialog(String message,
+                                                   DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .create()
+                .show();
     }
 }
