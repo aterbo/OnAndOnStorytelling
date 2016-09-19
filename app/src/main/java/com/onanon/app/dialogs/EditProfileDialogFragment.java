2ced2f6a20a743890aea2f39eeb54ba60edb59ee
ATerbo <andy.terbovich@gmail.com>
2016-09-18 21:59:26 -0700

    Changed dialog to use layout file and have image in it.

--- app/src/main/java/com/onanon/app/dialogs/EditProfileDialogFragment.java
+++ app/src/main/java/com/onanon/app/dialogs/EditProfileDialogFragment.java
@@ -6,23 +6,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+
+import com.onanon.app.R;
 
 /**
  * Created by ATerbo on 9/18/16.
  */
 public class EditProfileDialogFragment extends DialogFragment {
+
+    private EditText mEditText;
+
     @Override
-    public View onCreateView(
-            LayoutInflater inflater,
-            ViewGroup container,
-            Bundle savedInstanceState) {
-        LinearLayout linLayout =
-                new LinearLayout(getActivity());
-        Button b = new Button(getActivity());
-        b.setText("Hello Button");
-        linLayout.addView(b);
-        return linLayout;
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.dialog_edit_profile, container);
+        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
+        getDialog().setTitle("Hello");
+
+        return view;
     }
 
 }
