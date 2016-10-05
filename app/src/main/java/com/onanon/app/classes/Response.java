31bb0406d1958a1253d2eb23934e9b6f07365a4c
ATerbo <andy.terbovich@gmail.com>
2016-10-05 11:24:11 -0700

    Created Response class

---
+++ app/src/main/java/com/onanon/app/classes/Response.java
@@ -0,0 +1,89 @@
+package com.onanon.app.classes;
+
+/**
+ * Created by ATerbo on 10/5/16.
+ */
+public class Response {
+
+    public Response() { }
+
+    public Response(String originalTellerUserName, String responderUserName, String response,
+                    String originalConversationPushId, Prompt promptRespondingTo,
+                    long dateResponseSubmitted, long typeOfResponse) {
+        this.originalTellerUserName = originalTellerUserName;
+        this.responderUserName = responderUserName;
+        this.response = response;
+        this.originalConversationPushId = originalConversationPushId;
+        this.promptRespondingTo = promptRespondingTo;
+        this.dateResponseSubmitted = dateResponseSubmitted;
+        this.typeOfResponse = typeOfResponse;
+    }
+
+    private String originalTellerUserName;
+    private String responderUserName;
+    private String response;
+    private String originalConversationPushId;
+    private Prompt promptRespondingTo;
+    private long dateResponseSubmitted;
+    private long typeOfResponse;
+
+    private static long TEXT_RESPONSE = 0;
+    private static long AUDIO_RESPONSE = 1;
+    private static long VIDEO_RESPONSE = 2;
+
+    public String getOriginalTellerUserName() {
+        return originalTellerUserName;
+    }
+
+    public void setOriginalTellerUserName(String originalTellerUserName) {
+        this.originalTellerUserName = originalTellerUserName;
+    }
+
+    public String getResponderUserName() {
+        return responderUserName;
+    }
+
+    public void setResponderUserName(String responderUserName) {
+        this.responderUserName = responderUserName;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getOriginalConversationPushId() {
+        return originalConversationPushId;
+    }
+
+    public void setOriginalConversationPushId(String originalConversationPushId) {
+        this.originalConversationPushId = originalConversationPushId;
+    }
+
+    public Prompt getPromptRespondingTo() {
+        return promptRespondingTo;
+    }
+
+    public void setPromptRespondingTo(Prompt promptRespondingTo) {
+        this.promptRespondingTo = promptRespondingTo;
+    }
+
+    public long getDateResponseSubmitted() {
+        return dateResponseSubmitted;
+    }
+
+    public void setDateResponseSubmitted(long dateResponseSubmitted) {
+        this.dateResponseSubmitted = dateResponseSubmitted;
+    }
+
+    public long getTypeOfResponse() {
+        return typeOfResponse;
+    }
+
+    public void setTypeOfResponse(long typeOfResponse) {
+        this.typeOfResponse = typeOfResponse;
+    }
+}
